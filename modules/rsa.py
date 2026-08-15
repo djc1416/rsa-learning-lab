babdf8f4f0440602a00970f6196a86e383139dd7
djc1416 <dj.c.1416@gmail.com>
2026-08-15 17:46:30 -0400

    Add Rsa encryption and decryption

--- modules/rsa.py
+++ modules/rsa.py
@@ -11,3 +11,11 @@ def generate_keys(p, q, e):
 
     d = modular_inverse(e, phi)
     return (n, e), (n, d)  # public key, private key
+
+def encrypt(message, public_key):
+    n, e = public_key
+    return pow(message, e, n)
+
+def decrypt(ciphertext, private_key):
+    n, d = private_key
+    return pow(ciphertext, d, n)
--- test/test_rsa.py
+++ test/test_rsa.py
@@ -1,8 +1,17 @@
-from modules.rsa import generate_keys
+from modules.rsa import generate_keys, encrypt, decrypt
 
 def test_generate_keys():
     public_key, private_key = generate_keys(5, 11, 3)
 
     assert public_key == (55, 3)
     assert private_key == (55, 27)  
+
+def test_encrypt_and_decrypt():
+    public_key, private_key = generate_keys(5, 11, 3)
+    message = 7
+    ciphertext = encrypt(message, public_key)
+    decrypted_message = decrypt(ciphertext, private_key)
+
+    assert decrypted_message == message    
+
     
